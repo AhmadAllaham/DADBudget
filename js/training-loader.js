f39f6542893a57e2a938a245e9eb1308919da9df
AhmadAllaham <ahmad22allaham@gmail.com>
2026-08-23 18:01:03 +0300

    Remove unused Training loader

--- js/training-loader.js
+++
@@ -1,5 +0,0 @@
-(function(){
-'use strict';
-document.write('<script src="js/training-canonical-department-names.js?v=20260823-canonical-1"><\\/script>');
-document.write('<script src="js/training-allocations-core.js?v=20260823-core-1"><\\/script>');
-})();
