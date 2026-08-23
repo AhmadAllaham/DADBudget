0063ec64cb23e7c236450018830c4d67f4b94627
AhmadAllaham <ahmad22allaham@gmail.com>
2026-08-23 17:58:50 +0300

    Add synchronous Training canonical loader

---
+++ js/training-loader.js
@@ -0,0 +1,5 @@
+(function(){
+'use strict';
+document.write('<script src="js/training-canonical-department-names.js?v=20260823-canonical-1"><\\/script>');
+document.write('<script src="js/training-allocations-core.js?v=20260823-core-1"><\\/script>');
+})();
